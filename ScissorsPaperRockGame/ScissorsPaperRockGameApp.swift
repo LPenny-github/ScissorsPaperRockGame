4c257b3f3039d4a8fba246e91c2278d4f816e6a8
Penny Li <pylsu40@icloud.com>
2024-03-11 14:25:06 +0800

    Initial Commit

---
+++ ScissorsPaperRockGame/ScissorsPaperRockGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScissorsPaperRockGameApp.swift
+//  ScissorsPaperRockGame
+//
+//  Created by dev on 2024/3/11.
+//
+
+import SwiftUI
+
+@main
+struct ScissorsPaperRockGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
